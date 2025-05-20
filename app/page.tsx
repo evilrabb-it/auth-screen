52ab5e458e505b29d9a76a3c5050a3af99abe958
Evil Rabbit <adityakbare.connect@gmail.com>
2025-05-20 14:38:57 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import EvilRabbitAuthScreen from "../components/evil-rabbit-auth-screen"
+import EvilRabbitAuthScreen from "../evil-rabbit-auth-screen"
 
 export default function EvilRabbitLoginPage() {
   return <EvilRabbitAuthScreen />
